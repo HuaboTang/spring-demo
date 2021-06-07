1b28064b72e5db66a270701117a85ab011e73ad3
vbobot <tanghuabo1989@163.com>
2021-06-07 17:18:38 +0800

    change: 增加ThreadLocal相关的测试

--- spring-cloud-hystrix/spring-cloud-hystrix-client/src/main/java/com/vbobot/demo/spring/cloud/hystrix/client/DemoSpringCloudHystrixClientApplication.java
+++ spring-cloud-hystrix/spring-cloud-hystrix-client/src/main/java/com/vbobot/demo/spring/cloud/hystrix/client/DemoSpringCloudHystrixClientApplication.java
@@ -4,14 +4,12 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author Bobo
  * @date 2021/4/26
  */
-@EnableHystrix
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.vbobot.demo.spring.cloud.hystrix.api")
 @SpringBootApplication
