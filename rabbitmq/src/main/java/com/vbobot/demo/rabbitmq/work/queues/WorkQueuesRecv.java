37ae782df792bd8ecc0d2cab032ff15d1487c65d
vbobot <tanghuabo1989@163.com>
2021-06-23 10:18:06 +0800

    add: workqueues model

---
+++ rabbitmq/src/main/java/com/vbobot/demo/rabbitmq/work/queues/WorkQueuesRecv.java
@@ -0,0 +1,12 @@
+package com.vbobot.demo.rabbitmq.work.queues;
+
+/**
+ * @author Bobo
+ * @date 2021/6/22
+ */
+public class WorkQueuesRecv {
+
+    public static void main(String[] args) throws Exception {
+        RecvTask.process();
+    }
+}
