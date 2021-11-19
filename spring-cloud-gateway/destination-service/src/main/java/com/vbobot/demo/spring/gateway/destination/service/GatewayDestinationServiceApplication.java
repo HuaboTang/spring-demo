0cae24a1ad9215ba9173b4b883f6da0c48ca7849
bobo <tanghuabo@youxin.cloud>
2021-11-19 16:45:53 +0800

    tmp

--- spring-cloud-gateway/destination-service/src/main/java/com/vbobot/demo/spring/gateway/destination/service/GatewayDestinationServiceApplication.java
+++ spring-cloud-gateway/destination-service/src/main/java/com/vbobot/demo/spring/gateway/destination/service/GatewayDestinationServiceApplication.java
@@ -1,5 +1,6 @@
 package com.vbobot.demo.spring.gateway.destination.service;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +13,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class GatewayDestinationServiceApplication {
+    private final AtomicInteger count = new AtomicInteger(0);
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayDestinationServiceApplication.class, args);
+    }
+
+    @GetMapping("/reset")
+    public String reset() {
+        count.set(0);
+        return "reset";
+    }
+
+    @GetMapping("/sleep")
+    public String sleep() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(count.incrementAndGet());
+        return "sleep";
     }
 
     @GetMapping("/get")
--- spring-cloud-gateway/gateway-server/src/main/java/com/vbobot/demo/spring/cloud/gateway/gateway/server/GatewayServerApplication.java
+++ spring-cloud-gateway/gateway-server/src/main/java/com/vbobot/demo/spring/cloud/gateway/gateway/server/GatewayServerApplication.java
@@ -2,6 +2,7 @@ package com.vbobot.demo.spring.cloud.gateway.gateway.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.netty.ReactorNetty;
 
 /**
  * @author Bobo
@@ -11,6 +12,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GatewayServerApplication {
 
     public static void main(String[] args) {
+        System.setProperty(ReactorNetty.ACCESS_LOG_ENABLED, "false");
+
+        //https://blog.csdn.net/manzhizhen/article/details/115386684
+//        System.setProperty(ReactorNetty.NATIVE, "false");
+
+//        System.setProperty(ReactorNetty.IO_WORKER_COUNT, "16");
+//        System.setProperty(ReactorNetty.IO_SELECT_COUNT, "16");
+
+        System.setProperty("org.jboss.netty.epollBugWorkaround", "true");
+
         SpringApplication.run(GatewayServerApplication.class, args);
     }
 }
